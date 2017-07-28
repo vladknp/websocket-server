8801157c01b515b82088458b43ed977c38d1dbad
vladknp <knplink@gmail.com>
2017-07-28 16:04:21 +0300

    make push module

--- index.js
+++ index.js
@@ -7,18 +7,29 @@ var clients = {};
 var webSocketServer = new WebSocketServer.Server({
   port: 8081
 });
+
+var dataPlace = [];
+
+for (var i = 1; i < 4; i++) {
+  dataPlace[i] = generateData();
+}
+
+function pushData (param) {
+  // console.log(param)
+  param.clients[param.id].send(JSON.stringify(dataPlace[param.message]));
+}
 webSocketServer.on('connection', function(ws) {
 
   var id = Math.random();
   clients[id] = ws;
   console.log("новое соединение " + id);
 
-  ws.on('message', function(message) {
-    console.log('получено сообщение ' + message);
 
-    for (var key in clients) {
-      clients[key].send(message + "X");
-    }
+  ws.on('message', function(message) {
+    console.log(id, ': получено сообщение ' + message);
+
+    pushData({ clients, message, id });
+    
   });
 
   ws.on('close', function() {
@@ -27,3 +38,35 @@ webSocketServer.on('connection', function(ws) {
   });
 
 });
+
+function generateData () {
+  const arrPlaces = [...Array(Math.ceil(Math.random() * (1 + 5))).keys()].reduce((previousRack, currentRack, index) => { // 40
+    let parentIndex = currentRack
+    previousRack = [
+      ...previousRack,
+      {
+        id: `id-rake-${currentRack}`,
+        title: `rake-${currentRack}`,
+        nodes: (function () {
+          return [...Array(Math.ceil(Math.random() * (15 + 15))).keys()].reduce((previousNode, current, nodeId) => {
+            previousNode = [
+              ...previousNode,
+              {
+                id: `id-${currentRack}-${nodeId}`,
+                ip: Math.ceil(Math.random() * (1 + 1000)),
+                title: `${parentIndex}c${current}p`,
+                status: Math.ceil(Math.random() * (0 + 2)) === 1 ? 'on' : 'off'
+              }
+            ]
+
+            return previousNode
+          }, [])
+        })()
+      }
+    ]
+
+    return previousRack
+  }, [])
+
+  return arrPlaces
+}
